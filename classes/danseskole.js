22a91f617f15cd8f3559791012c73b9c9e4d5fd6
Luka <lupa19ac@student.cbs.dk>
2019-10-09 15:34:19 +0200

    Opdateret danseskole

---
+++ classes/danseskole.js
@@ -0,0 +1,11 @@
+//Starter med at ærklære en klasse.
+//Derefter definerer jeg metoderne (typer).
+//Nøgleordet class danseskole starter en contructer som tillader os at definere typer og et sæt af metoder, samlede på et sted.
+class Danseskole {
+    constructor(adresse, undervisere, events, lokaler) {
+        this.adresse = adresse;
+        this.undervisere = undervisere;
+        this.events = events;
+        this.lokaler = lokaler;
+    }
+}
