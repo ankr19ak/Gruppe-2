ff7724cd0b1c027ed3a43f9f34e873d7e4fac510
magnuslanther <55132227+magnuslanther@users.noreply.github.com>
2019-10-09 15:20:32 +0200

    user, kunde og undervisningstimer klasser

---
+++ classes/kunde.js
@@ -0,0 +1,11 @@
+class kunde {
+    constructor(username, password, navn, fødselsår, køn, mobilTlf, email) {
+        this.username = username;
+        this.password = password;
+        this.navn = navn;
+        this.fødselsår = fødselsår;
+        this.køn = køn;
+        this.mobilTlf = mobilTlf;
+        this.email = email;
+    }
+}
