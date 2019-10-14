bffdd984de151a6e9233104fb4acb10a944c654f
magnuslanther <55132227+magnuslanther@users.noreply.github.com>
2019-10-14 08:32:46 +0200

    Ændret variablernes navne til engelsk

--- classes/undervisningstimer.js
+++ classes/undervisningstimer.js
@@ -1,12 +1,12 @@
 //Klassen undervisningstimer erklæres
 class undervisningstimer {
-    constructor(titel, lokale, tid, varighed, deltagere, underviser) {
-        this.titel = titel;
-        this.lokale = lokale;
-        this.tid = tid;
-        this.varighed = varighed;
-        this.deltagere = deltagere;
-        this.underviser = underviser;
+    constructor(title, location, time, duration, participants, teacher) {
+        this.title = title;
+        this.location = location;
+        this.time = time;
+        this.duration = duration;
+        this.participants = participants;
+        this.teacher = teacher;
     }
 }
 
