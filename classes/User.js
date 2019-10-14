25242260dc8d7f135e761e9de8c3f011f7ce47f8
Andreas Wederkinck <andreaswederkinck@Andreass-MacBook-Pro-2.local>
2019-10-14 15:47:02 +0200

    endelig fikset bruger til array

--- classes/User.js
+++ classes/User.js
@@ -6,51 +6,58 @@ class User {
     }
 }
 
-const allUsers = [];
-
+    allUsers = [];
     function createUser() {
-        var user = new User(document.getElementById('user').value, document.getElementById('password').value);
+        let user = new User(document.getElementById('user').value, document.getElementById('password').value);
 
         allUsers.push(user);
-        showAllUsers();
+        console.log(allUsers);
+
     }
 
-    function showAllUsers(){
-        console.log(allUsers);
-    }
-// Store input from registration form in localStorage
-    /*storeLogin() {
-        localStorage.setItem('username', username.value);
-        localStorage.setItem('password', password.value);
-        alert('New user has been created.');
-    }
+function storeLogin() {
+    localStorage.setItem('username', username.value);
+    localStorage.setItem('password', password.value);
+    alert('New user has been created.');
+}
 
 // Function to list user in localStorage
-    function
+function showUser() {
+console.log('Show user stored in localStorage.');
+console.log(localStorage);
+}
 
-    showUser() {
-        console.log('Show user stored in localStorage.');
-        console.log(localStorage);
-    }
+function showUser() {
+    console.log('Show user stored in localStorage.');
+    console.log(localStorage);
+}
 
-    function
-
-    checkLogin() {
-
+function checkLogin() {
+    checkLogin();
+    {
+// Stored data from the registration form
+        var storedName = localStorage.getItem('username');
+        var storedPass = localStorage.getItem('password');
         // Stored data from the registration form
         var storedName = localStorage.getItem('username');
         var storedPass = localStorage.getItem('password');
-
+// Entered data in the login form
+        var enteredName = document.getElementById('enteredName');
+        var enteredPass = document.getElementById('enteredPass');
         // Entered data in the login form
         var enteredName = document.getElementById('enteredName');
         var enteredPass = document.getElementById('enteredPass');
-
-        // Check if stored data from registration form is equal to entered data from login form
+// Check if stored data from registration form is equal to entered data from login form
         if (enteredName.value == storedName && enteredPass.value == storedPass) {
             alert('You are logged in.');
         } else {
             alert('Error. Wrong login.');
+            // Check if stored data from registration form is equal to entered data from login form
+            if (enteredName.value == storedName && enteredPass.value == storedPass) {
+                alert('You are logged in.');
+            } else {
+                alert('Error. Wrong login.');
+            }
         }
     }
-
-    */
+}
