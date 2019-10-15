b6dd89ecc145da537175cd9ea3a72939ba9e8abf
magnuslanther <55132227+magnuslanther@users.noreply.github.com>
2019-10-15 11:26:06 +0200

    Krav til registrering

--- classes/User.js
+++ classes/User.js
@@ -8,14 +8,18 @@ class User {
 
     allUsers = [];
     function createUser() {
+        if (checkAtSign() === true
+            && checkPassNum() === true
+            && checkNumberLength() === true
+        ){
             let user = new User(
                 document.getElementById('user').value,
                 document.getElementById('password').value
             );
             allUsers.push(user);
             console.log(allUsers);
+        }
     }
-
 function storeLogin() {
     localStorage.setItem('username', username.value);
     localStorage.setItem('password', password.value);
@@ -70,4 +74,21 @@ function checkAtSign(){
     }
         else alert("This doesn't look like an email");
             return false;
+}
+
+// En funktion som tjekker om password er mindst 6 cifre
+function checkPassNum() {
+    if(document.getElementById('password').value.length >= 6) {
+        return true;
+    }
+    else alert("Your password is too short");
+    return false;
+}
+// En funktion som tjekker om telefon nummeret er 8 cifre
+function checkNumberLength() {
+    if(document.getElementById('phoneNumber').value.length === 8) {
+        return true;
+    }
+    else alert("Not right phone number")
+    return false;
 }
