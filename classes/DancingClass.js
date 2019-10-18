10ca6d4bdf6d8ef8d7bb6699e99cf0869d3e8e26
Andreas Wederkinck <andreaswederkinck@Andreass-MacBook-Pro-2.local>
2019-10-18 14:14:49 +0200

    Man kan se alle undervisningstimer

--- classes/DancingClass.js
+++ classes/DancingClass.js
@@ -1,39 +1,53 @@
 //Klassen undervisningstimer erklæres
 class DancingClass {
-    constructor(title, location, room, teacher, durationHours, durationMinutes){
+    constructor(title, location, room, teacher, participants, durationHours, durationMinutes){
         this.title = title;
         this.location = location;
         this.room = room;
         this.teacher = teacher;
         this.durationHours = durationHours;
         this.durationMinutes = durationMinutes;
+        this.participants = participants;
     }
 }
 
 //Laver en array af ny klasser der bliver oprettet og henter title, location, room, teacher vha. .getElementById.
 allClasses = [
     {
-        title: "Undervisningstime1",
+        title: "Undervisningstime 1",
         location: "Solbjerg Plads 15",
         room: 156,
         teacher: "Jan Jensen",
         durationHours: 1,
-        durationMinutes: 30
+        durationMinutes: 30,
+        participants: [
+            {
+                username: "AndreasWK",
+                password: "hej123",
+                name: "Andreas",
+                birthYear: 1997,
+                gender: "Male",
+                phoneNumber: 61795436,
+                email: "andreas@ersej.dk"
+            }
+            ]
     },
     {
-        title: "Undervisningstime2",
+        title: "Undervisningstime 2",
         location: "Dalgas Have 3",
         room: 254,
         teacher: "Erik Eriksen",
         durationHours: 0,
-        durationMinutes: 45
+        durationMinutes: 45,
+        participants: []
     },
     {
-        title: "Undervisningtime3",
+        title: "Undervisningtime 3",
         location: "Peter Bangs Vej 20",
         room: 10,
         durationHours: 2,
-        durationMinutes: 20
+        durationMinutes: 20,
+        participants: []
     }
 ];
 function createDancingClass() {
@@ -58,6 +72,44 @@ function createDancingClass() {
         allClasses.push(dancingClass);
         alert('You have created a new class.');
         console.log(allClasses);
+    }
+}
+
+function showAllClasses() {
+    for(var n = 0; n < allClasses.length; n++){
+
+        var header = document.createElement("h2");
+        var headerText = document.createTextNode(allClasses[n].title);
+        header.append(headerText);
+        document.getElementById("body").append(header);
+
+        var paraSted = document.createElement("P");
+        var textSted = document.createTextNode("Sted: " + allClasses[n].location);
+        paraSted.appendChild(textSted);
+        document.getElementById("body").append(paraSted);
+
+        var paraRoom = document.createElement("P");
+        var textRoom = document.createTextNode("Lokale: " + allClasses[n].room);
+        paraRoom.appendChild(textRoom);
+        document.getElementById("body").appendChild(paraRoom);
+
+        var paraTeacher = document.createElement("P");
+        var textTeacher = document.createTextNode("Underviser: " + allClasses[n].teacher);
+        paraTeacher.appendChild(textTeacher);
+        document.getElementById("body").appendChild(paraTeacher);
+
+        var paraDuration = document.createElement("P");
+        var textDuration = document.createTextNode("Varighed: " + allClasses[n].durationHours + " time(r) og " + allClasses[n].durationMinutes + " minutter");
+        paraDuration.appendChild(textDuration);
+        document.getElementById("body").appendChild(paraDuration);
+
+        var paraParticipants = document.createElement("P");
+        var textParticipants= document.createTextNode("Deltagere: " + allClasses[n].participants);
+        paraParticipants.appendChild(textParticipants);
+        document.getElementById("body").appendChild(paraParticipants);
+
+        var lineBreak = document.createElement("br");
+        document.getElementById("body").appendChild(lineBreak);
     }
 }
 
