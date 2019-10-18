995e90a834f1518c86ed8f833a1296a75a8a9b8b
Luka <lupa19ac@student.cbs.dk>
2019-10-18 09:10:51 +0200

    har bl.a. ændret createclassfuktionen

--- classes/DancingClass.js
+++ classes/DancingClass.js
@@ -9,7 +9,29 @@ class DancingClass {
     }
 }
 
-allClasses = [];
+//Laver en array af ny klasser der bliver oprettet og henter title, location, room, teacher og duration vha. .getElementById.
+allClasses = [
+    {
+        title: "Undervisningstime1",
+        location: "Solbjerg Plads 15",
+        room: 156,
+        teacher: "Jan Jensen",
+        duration: 100
+    },
+    {
+        title: "Undervisningstime2",
+        location: "Dalgas Have 3",
+        room: 254,
+        teacher: "Erik Eriksen",
+        duration: 45
+    },
+    {
+        title: "Undervisningtime3",
+        location: "Peter Bangs Vej 20",
+        room: 10,
+        duration: 200
+    }
+];
 function createDancingClass(){
     let dancingClass = new DancingClass(
         document.getElementById("title").value,
@@ -18,24 +40,17 @@ function createDancingClass(){
         document.getElementById("teacher").value,
         document.getElementById("duration").value
     );
+
+    //Klasserne bliver pushet op og logget ind i consolen.
     allClasses.push(dancingClass);
-    storeClass();
+    alert('You have created a new class.');
     console.log(allClasses);
 }
 
-function storeClass(){
-    localStorage.setItem("title", title.value);
-    localStorage.setItem("location", location.value);
-    localStorage.setItem("room", room.value);
-    localStorage.setItem("teacher", teacher.value);
-    localStorage.setItem("duration", duration.value);
-    alert('You have created a new class.');
-}
-
+//Klasserne bliver vist i localstorage.
 function showClass(){
     console.log("Show new class stored in localStorage");
     console.log(localStorage);
 }
-
 
 
